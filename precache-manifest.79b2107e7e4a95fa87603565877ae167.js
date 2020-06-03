d374050f519093c0310206e5b9765bfa97b6b093
Yagel <yagel@brounie.com>
2020-06-02 22:27:43 -0500

    Updates

---
+++ precache-manifest.79b2107e7e4a95fa87603565877ae167.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4205591b1cde3af1adc57327662dc70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2081b7f47d4d0b9ce7e",
+    "url": "/static/css/main.4781c77a.chunk.css"
+  },
+  {
+    "revision": "e5df1b28327ed996835f",
+    "url": "/static/js/2.878d764c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.878d764c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2081b7f47d4d0b9ce7e",
+    "url": "/static/js/main.e02fa88f.chunk.js"
+  },
+  {
+    "revision": "6f7b1e814e9f4cd5504f",
+    "url": "/static/js/runtime-main.98102d87.js"
+  },
+  {
+    "revision": "caa51169682ca650f7b9c7295c65268e",
+    "url": "/static/media/acc-screens.caa51169.png"
+  },
+  {
+    "revision": "36ff8d7ae416318d4f6e1a361566b08b",
+    "url": "/static/media/buntiapp.36ff8d7a.png"
+  },
+  {
+    "revision": "fd75cadfd52031c605e6654462b9dbec",
+    "url": "/static/media/ddManufacture.fd75cadf.png"
+  },
+  {
+    "revision": "d9e8d42f15e3ef10b62a0fec1f2831f4",
+    "url": "/static/media/lolKioru.d9e8d42f.png"
+  },
+  {
+    "revision": "a99a0971912d87c3ebaf2aae3a3d284d",
+    "url": "/static/media/mernTasks.a99a0971.png"
+  },
+  {
+    "revision": "9ed3646ad4468249ba730c82eec9e27c",
+    "url": "/static/media/piedra-papel-tijera.9ed3646a.png"
+  },
+  {
+    "revision": "5bcc8a862110081261f9d5ce72b5d239",
+    "url": "/static/media/porfile.5bcc8a86.jpg"
+  },
+  {
+    "revision": "9eb52e563350777d541d5f232d756e5c",
+    "url": "/static/media/pruebaBrounieAndroid.9eb52e56.png"
+  },
+  {
+    "revision": "777b4ffa19d18b5d39d79007e8671000",
+    "url": "/static/media/resume.777b4ffa.png"
+  },
+  {
+    "revision": "bc0dbeadb21ba8ee09be808932fee6a8",
+    "url": "/static/media/task-manager.bc0dbead.png"
+  }
+]);
